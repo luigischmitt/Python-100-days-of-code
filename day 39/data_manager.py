4079b12409aafe461784722f911457341394cb59
luigischmitt <schmittluigi@gmail.com>
2024-05-31 18:11:24 -0300

    feat: add day 39

---
+++ day 39/data_manager.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/e18a6afc12b8496aa4e88cb6e83bb814/flightDealsProject/prices"
+
+class DataManager:
+
+    def __init__(self):
+        self._user = os.environ["SHEETY_USERNAME"]
+        self._password = os.environ["SHEETY_PASSWORD"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+    
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
+
---
+++ day 39/main.py
@@ -0,0 +1,62 @@
+import time
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import FlightData
+from notification_manager import NotificationManager
+
+# ==================== Set up the Flight Search ====================
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+
+# Set your origin airport
+ORIGIN_CITY_IATA = "LON"
+
+# ==================== Update the Airport Codes in Google Sheet ====================
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        # slowing down requests to avoid rate limit
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
+
+# ==================== Search for Flights ====================
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = FlightData.find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slowing down requests to avoid rate limit
+    time.sleep(2)
+
+# ========================= SMS ALERT ============================
+
+if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        NotificationManager.send_sms(
+            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
+        
+        # SMS not working? Try whatsapp instead.
+        # NotificationManager.send_whatsapp(
+        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
