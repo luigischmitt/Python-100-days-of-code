4788f8f170b4d789bfce4bbce342e6be97fa2733
luigischmitt <schmittluigi@gmail.com>
2024-06-18 14:37:32 -0300

    feat: add day 51

---
+++ day 51/main.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+TWITTER_EMAIL = ""
+TWITTER_PASS = ""
+USERNAME = ""
+URL = "https://twitter.com/i/flow/signup"
+
+# ---------------------------------------------------------------------------------------------------------------------
+URL2 = "https://www.speedtest.net/"
+
+# Keep the browser open (helps diagnose issues if the script crashes)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(URL2)
+
+# Starts
+sleep(2)
+go_but = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a')
+go_but.click()
+sleep(25)
+download_speed = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span')
+speed = download_speed.text
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+# Keep the browser open (helps diagnose issues if the script crashes)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(URL)
+
+# Login 
+sleep(2)
+login_but = driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/button/span/span')
+login_but.click()
+sleep(4)
+email_input = driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input')
+email_input.click()
+email_input.send_keys(TWITTER_EMAIL)
+submit_but = driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]/div')
+sleep(1)
+submit_but.click()
+sleep(1)
+username_input = driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')
+username_input.send_keys(USERNAME)
+next_but = driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/button/div')
+next_but.click()
+sleep(1)
+password_input = driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+password_input.send_keys(TWITTER_PASS)
+login = driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/button/div')
+login.click()
+
+# Internet speed test
+if float(speed) < 250:
+    sleep(4)
+    quote_input = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+    quote_input.send_keys(f"My internet Speed is below the average, download speed: {speed} Mbps! help me @Brisanet")
+    post_but = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/button')
+    post_but.click()
+else:
+    print("Everything is fine.")
+
